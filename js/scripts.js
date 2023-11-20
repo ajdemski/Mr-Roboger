e34b578d1b7891ff50e37cbb3fe79f344473d11e
Aaron Demski <ajdemski@frontier.com>
2023-11-19 19:52:01 -0800

    is now working properly

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,11 @@ function generateRoboger(number) {
   const result = [];
   for (let i = 0; i <= number; i++) {
     let message = "";
-    if (i.toString().includes("1")) {
+    if (i.toString().includes("3")) {
       message += "won't you be my neighbor?";
     } else if (i.toString().includes("2")) {
       message += "boop";
-    } else if (i.toString().includes("3")) {
+    } else if (i.toString().includes("1")) {
       message += "beep";
     } else {
       message = i.toString();
